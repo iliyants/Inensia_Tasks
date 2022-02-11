93a0123f87a245ecbf6e8c41d40c6f41c30a563e
iliyants <58304312+iliyants@users.noreply.github.com>
2022-02-11 13:52:34 +0200

    Add files via upload

---
+++ Task2/Task2/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Task2;
+
+internal class Startup
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Please enter a gross salary: ");
+
+        decimal grossValue;
+        var input = Console.ReadLine();
+
+
+        while(!decimal.TryParse(input, out grossValue) || decimal.Parse(input) < 0)
+        {
+            Console.WriteLine("Please enter a valid positive number.");
+
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine($"The net salary is {TaxCalculator.CalculateNetValue(grossValue)} IDR.");
+
+    }
+}
